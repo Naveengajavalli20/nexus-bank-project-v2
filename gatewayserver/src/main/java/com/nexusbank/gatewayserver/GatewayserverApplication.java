5e2558c89105ef525b395edaaa4fdb86b32546b6
Naveen <naveengajavalli20@gmail.com>
2026-03-24 19:36:33 +0530

    initial commit for version-2

---
+++ gatewayserver/src/main/java/com/nexusbank/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,41 @@
+package com.nexusbank.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator nexusbankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+						.route(p -> p
+								.path("/nexusbank/accounts/**")
+								.filters( f -> f.rewritePath("/nexusbank/accounts/(?<segment>.*)","/${segment}")
+										.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.uri("lb://ACCOUNTS"))
+					.route(p -> p
+							.path("/nexusbank/loans/**")
+							.filters( f -> f.rewritePath("/nexusbank/loans/(?<segment>.*)","/${segment}")
+									.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+							.uri("lb://LOANS"))
+					.route(p -> p
+							.path("/nexusbank/cards/**")
+							.filters( f -> f.rewritePath("/nexusbank/cards/(?<segment>.*)","/${segment}")
+									.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+							.uri("lb://CARDS")).build();
+
+
+	}
+
+
+}
