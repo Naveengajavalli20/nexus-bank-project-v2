5c212cfce5a4ee812664794dc8be661c3a5327da
Naveen <naveengajavalli20@gmail.com>
2026-03-25 10:57:22 +0530

    added gitignore and feignclients

--- gatewayserver/src/main/java/com/nexusbank/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/nexusbank/gatewayserver/GatewayserverApplication.java
@@ -11,31 +11,32 @@ import java.time.LocalDateTime;
 @SpringBootApplication
 public class GatewayserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayserverApplication.class, args);
+    }
 
-	@Bean
-	public RouteLocator nexusbankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
-		return routeLocatorBuilder.routes()
-						.route(p -> p
-								.path("/nexusbank/accounts/**")
-								.filters( f -> f.rewritePath("/nexusbank/accounts/(?<segment>.*)","/${segment}")
-										.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
-								.uri("lb://ACCOUNTS"))
-					.route(p -> p
-							.path("/nexusbank/loans/**")
-							.filters( f -> f.rewritePath("/nexusbank/loans/(?<segment>.*)","/${segment}")
-									.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
-							.uri("lb://LOANS"))
-					.route(p -> p
-							.path("/nexusbank/cards/**")
-							.filters( f -> f.rewritePath("/nexusbank/cards/(?<segment>.*)","/${segment}")
-									.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
-							.uri("lb://CARDS")).build();
+    @Bean
+    public RouteLocator nexusbankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/nexusbank/accounts/**")
+                        .filters(f -> f.rewritePath("/nexusbank/accounts/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("accountsCircuitBreaker")))
+                        .uri("lb://ACCOUNTS"))
+                .route(p -> p
+                        .path("/nexusbank/loans/**")
+                        .filters(f -> f.rewritePath("/nexusbank/loans/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://LOANS"))
+                .route(p -> p
+                        .path("/nexusbank/cards/**")
+                        .filters(f -> f.rewritePath("/nexusbank/cards/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://CARDS")).build();
 
 
-	}
+    }
 
 
 }
