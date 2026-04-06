46117b58f3909330ba10b755be576fde814e346a
Naveen <naveengajavalli20@gmail.com>
2026-04-06 17:13:45 +0530

    keycloak authentication

---
+++ gatewayserver/src/main/java/com/nexusbank/gatewayserver/config/JwtDecoderConfig.java
@@ -0,0 +1,19 @@
+package com.nexusbank.gatewayserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+
+@Configuration
+public class JwtDecoderConfig {
+
+    @Bean
+    @ConditionalOnMissingBean(ReactiveJwtDecoder.class)
+    public ReactiveJwtDecoder reactiveJwtDecoder(
+            @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") String jwkSetUri) {
+        return NimbusReactiveJwtDecoder.withJwkSetUri(jwkSetUri).build();
+    }
+}
